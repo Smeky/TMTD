a12287998bb756713f0190232eb5776775fae0d5
Smeky <smeky.sobota@gmail.com>
2021-01-25 21:52:17 +0100

    Added a way to switch between scenes

--- src/game.js
+++ src/game.js
@@ -19,9 +19,35 @@ class Game {
 
         }
 
+        this.setupScene()
+    }
+
+    setupScene() {
         // Scene init should be last
         this.sceneHandler = new SceneHandler()
         this.sceneHandler.setScene(EditorScene)
+
+        // Buttons to switch between scenes 
+        {
+            const level = new pixi.Container()
+            level.position.x = -90
+            level.position.y = -window.innerHeight / 2 + 30
+            level.interactive = true
+            level.on("mouseup", () => this.sceneHandler.setScene(LevelScene))
+            level.addChild(new pixi.Text("Level Scene", {fill: "#ffffff"}))
+            level.pivot.x = level.getLocalBounds().width / 2
+    
+            const editor = new pixi.Container()
+            editor.position.x = 90
+            editor.position.y = -window.innerHeight / 2 + 30
+            editor.interactive = true
+            editor.on("mouseup", () => this.sceneHandler.setScene(EditorScene))
+            editor.addChild(new pixi.Text("Editor Scene", {fill: "#ffffff"}))
+            editor.pivot.x = editor.getLocalBounds().width / 2
+            
+            this.graphics.stage.addChild(level)
+            this.graphics.stage.addChild(editor)
+        }
     }
     
     run() {
