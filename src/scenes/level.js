a2bea671755b5ab68f4031906bb9cf59a23a7cd1
Martin Mrazek <mrazekmart@gmail.com>
2021-02-01 18:07:07 +0100

    new: pathfinder.js
    - added pathFinder class
    - extended level scene with pathfinder

--- src/scenes/level.js
+++ src/scenes/level.js
@@ -1,9 +1,10 @@
-import {TilePalette} from "game/core/palette"
-import {Tile} from "game/core/tile"
+import { TilePalette } from "game/core/palette"
+import { Tile } from "game/core/tile"
 import Scene from "game/scenes/scene"
 import { Vec2 } from "game/core/structs"
 import * as pixi from "pixi.js"
 import { Entities } from "game/entity/entities"
+import { PathFinder } from "../core/pathfinder"
 
 export default class LevelScene extends Scene {
     constructor() {
@@ -14,12 +15,21 @@ export default class LevelScene extends Scene {
         this.entities = new Entities()
 
         this.setupGrid()
+        this.path();
         this.createEntity()
-        
+
         this.cd = 0.7
         this.cdProgress = 0.0
     }
-    
+    path() {
+        this.pathFinder = new PathFinder(this.availableTiles,
+            { x: 5, y: 0 },
+            { x: 12, y: 12 },
+            this.gridContainer.pivot)
+        this.pathFinder.findPath()
+        this.finalPath = this.pathFinder.getPath();
+    }
+
     close() {
         this.inputProxy.close()
     }
@@ -28,6 +38,7 @@ export default class LevelScene extends Scene {
         this.gridWidth = 16
         this.gridHeight = 13
 
+        this.availableTiles = []
         this.grid = []
         this.gridContainer = new pixi.Container()
         this.gridContainer.pivot.x = (this.gridWidth * Tile.Size) / 2
@@ -37,18 +48,18 @@ export default class LevelScene extends Scene {
 
         for (let y = 0; y < this.gridHeight; y++) {
             for (let x = 0; x < this.gridWidth; x++) {
-                if ((x >= 4  && x <= 5  && y >= 0 && y <= 5) ||
-                    (x >= 4  && x <= 12 && y >= 5 && y <= 6) ||
-                    (x >= 11 && x <= 12 && y >= 6 && y <= 13)) 
-                {
+                if ((x >= 4 && x <= 5 && y >= 0 && y <= 5) ||
+                    (x >= 4 && x <= 12 && y >= 5 && y <= 6) ||
+                    (x >= 11 && x <= 12 && y >= 6 && y <= 13)) {
                     this.palette.selectTile(11)
-                } 
+                    this.availableTiles.push({ x: x, y: y })
+                }
                 else {
                     this.palette.selectTile(4)
                 }
 
                 const sprite = new pixi.Sprite(this.palette.getSelectedTileTexture())
-                
+
                 sprite.width = Tile.Size
                 sprite.height = Tile.Size
                 sprite.x = x * Tile.Size
@@ -66,12 +77,12 @@ export default class LevelScene extends Scene {
 
     createEntity() {
         // Temporary, duh :D 
-        const {pivot} = this.gridContainer
-        const path = [
-            new Vec2(5  * Tile.Size - pivot.x, 6  * Tile.Size - pivot.y),
-            new Vec2(12 * Tile.Size - pivot.x, 6  * Tile.Size - pivot.y),
-            new Vec2(12 * Tile.Size - pivot.x, 13 * Tile.Size - pivot.y),
-        ]
+        const { pivot } = this.gridContainer
+        /*         const path = [
+                    new Vec2(5  * Tile.Size - pivot.x, 6  * Tile.Size - pivot.y),
+                    new Vec2(12 * Tile.Size - pivot.x, 6  * Tile.Size - pivot.y),
+                    new Vec2(12 * Tile.Size - pivot.x, 13 * Tile.Size - pivot.y),
+                ] */
 
         const entity = this.entities.createEntity()
 
@@ -87,7 +98,7 @@ export default class LevelScene extends Scene {
 
         const movement = entity.addComponent("movement")
         movement.speed = 50
-        movement.destinations = movement.destinations.concat(path)
+        movement.destinations = movement.destinations.concat(this.finalPath)
 
         this.entities.initEntity(entity)
     }
@@ -99,7 +110,7 @@ export default class LevelScene extends Scene {
             this.cdProgress += delta
             if (this.cdProgress >= this.cd) {
                 this.cdProgress %= this.cd
-    
+
                 this.createEntity()
             }
         }
