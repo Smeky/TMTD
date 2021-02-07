a760974e004e8d42c9c7efac5abe1f267ddd56d7
Smeky <smeky.sobota@gmail.com>
2021-02-07 17:19:49 +0100

    Added first signs of a tower

--- src/scene/level.js
+++ src/scene/level.js
@@ -1,19 +1,25 @@
 import { Grid } from "game/core/grid"
 import { Tile } from "game/core/tile"
 import { Scene } from "game/scene"
-import { Vec2 } from "game/core/structs"
+import { Rect, Vec2 } from "game/core/structs"
 import * as pixi from "pixi.js"
 import { ECS } from "game/entity/entities"
 import * as pf from "game/core/pathfinding"
+import utils from "game/utils"
+
+const TowerSize = 50
 
 export default class LevelScene extends Scene {
     constructor() {
         super("level")
+
+        this.sceneContainer.interactive = true
+        this.sceneContainer.on("mouseup", this.handleMouseUp)
         
         // Todo: Please, please, need a better solution
         // Setup layers so we can control zIndex better
         this.sceneContainer.sortableChildren = true
-        this.containers = ["grid", "entities", "healthbars"]
+        this.containers = ["grid", "towers", "entities", "healthbars"]
             .map((name, index) => {
                 const container = new pixi.Container()
                 
@@ -29,7 +35,6 @@ export default class LevelScene extends Scene {
 
         this.inputProxy = game.input.getProxy()
         this.entities = new ECS()
-
 
         this.started = false
         this.load()
@@ -109,5 +114,61 @@ export default class LevelScene extends Scene {
                 this.createEntity()
             }
         }
+    }
+
+    createTower(pos) {
+        const snapped = this.grid.snapPosToTile(pos)
+        const tiles = this.grid.getTilesByBounds(new Rect(snapped.x + 1, snapped.y + 1, TowerSize, TowerSize))
+                               .filter(tile => !this.grid.isTileObstructed(tile))
+        
+        if (tiles.length < 4) {
+            console.warn("Can not build here", pos)
+            return
+        }
+        else {
+            // Todo: tiles should be immutable, this should be done via the grid
+            for (const tile of tiles) {
+                tile.isBlocked = true
+            }
+        }
+
+        const baseBounds = new Rect(0, 0, TowerSize, TowerSize)
+        const baseTexture = utils.createRectTexture(baseBounds, 0xff0000)
+        const baseSprite = new pixi.Sprite(baseTexture)
+
+        this.containers.towers.addChild(baseSprite)
+
+        // Todo: fix this when we fix grid indices
+        // Just find topleft tile.. 
+        const topleft = tiles.reduce((candidate, tile) => {
+            if (!candidate) {
+                candidate = tile
+            }
+            else {
+                if (tile.x < candidate.x || tile.y < candidate.y) {
+                    candidate = tile
+                }
+            }
+
+            return candidate
+        }, null)
+
+
+        const components = {
+            "transform": {
+                pos: topleft.pos.add(Tile.Size - TowerSize / 2)
+            },
+            "display": {
+                object: baseSprite
+            }
+        }
+
+        const entity = this.entities.createEntity(components)
+        this.entities.initEntity(entity)
+    }
+
+    handleMouseUp = (event) => {
+        const pos = new Vec2(event.data.getLocalPosition(this.sceneContainer))
+        this.createTower(pos)
     }
 }
